4a9cfb60f54eeed5fc8fdd57f1919d8e27a1d892
derekfulmer <derek.m.fulmer@gmail.com>
2020-09-20 19:52:20 -0500

    Renamed main module file. Cleaning up repo.

---
+++ auditor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+''' 
+Rewrite github_audit.sh in Python 3.8.2 using the requests modle to make API calls to GitHub. 
+A module to process text similar to the combination of grep/sed/awk.
+Store credentials as environment variables and call them from the script
+Publish new file to S3 bucket?
+'''
+
+
