f44f1bfc36c95be36f2b1cd50676b5f828265e74
derekfulmer <derek.m.fulmer@gmail.com>
2020-09-21 20:44:41 -0500

    Fleshing out ideas for how the script should behave. Mostly psuedocode at this point.

--- auditor.py
+++ auditor.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
+import requests
+import os
+
 ''' 
 Rewrite github_audit.sh in Python 3.8.2 using the requests modle to make API calls to GitHub. 
 A module to process text similar to the combination of grep/sed/awk.
 Store credentials as environment variables and call them from the script
-Publish new file to S3 bucket?
+Publish new file to S3 bucket.
 '''
+
+def fetch_users():
+	url = 'https://api.github.com/orgs/ORG/members'
+	headers = {'application': 'vnd.github.wyandotte-preview+json'}
+
+	# Basic functionality below to show ability to hit API endpoint.
+	r = requests.get(url, headers)
+
+	# Function to fetch GitHub user accounts.
+	# Get all users, parse JSON to return only IDs
+	# return output to function caller?
+	# Write data pulled from API to a file within a context manager?
+	pass
 
 
