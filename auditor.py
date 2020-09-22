69d1c7db79c13cafd073f46fc08fb8e0bb6f4c44
derekfulmer <derek.m.fulmer@gmail.com>
2020-09-21 21:29:01 -0500

    Testing dotenv library to grab credentials.

--- auditor.py
+++ auditor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import requests
 import os
+import requests
+from dotenv, import load_env
 
 ''' 
 Rewrite github_audit.sh in Python 3.8.2 using the requests modle to make API calls to GitHub. 
@@ -9,6 +10,11 @@ A module to process text similar to the combination of grep/sed/awk.
 Store credentials as environment variables and call them from the script
 Publish new file to S3 bucket.
 '''
+
+def get_creds():
+	username = os.getenv('GITHUB_USERNAME')
+	token = os.getenv('GITHUB_TOKEN')
+	return username token
 
 def fetch_users():
 	url = 'https://api.github.com/orgs/ORG/members'
