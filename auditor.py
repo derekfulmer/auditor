d7f99eb84df028e32d71bec15f6ac8b4bbc09917
derekfulmer <derek.m.fulmer@gmail.com>
2021-01-28 15:23:38 -0600

    Structuring as function to possibly port this to a CLICK based CLI app.

--- auditor.py
+++ auditor.py
@@ -5,7 +5,7 @@ import logging
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
 ''' 
 Rewrite github_audit.sh in Python 3.8.2 using the requests modle to make API calls to GitHub. 
@@ -17,14 +17,19 @@ log.setLevel(logging.INFO)
 
 # GitHub credentials should be stored externally from script in a secrets.env file or similar.
 try:
+    load_dotenv()
     username = os.getenv('GITHUB_USERNAME')
     token = os.getenv('GITHUB_TOKEN')
 except:
     logging.critical("ERROR: Have your GitHub credentials been exported as environment variables? You need your GitHub username and a Personal Acces Token which acts as your password.")
     exit(1)
 
-url = 'https://api.github.com/orgs/BLC/members'
-auth = requests.auth.HTTPBasicAuth(username, token)
+def fetch_users():
+	url = 'https://api.github.com/orgs/ORG/members'
+	auth = requests.auth.HTTPBasicAuth(username, token)
+	
+	r = requests.get(url, auth=auth)
 
-r = requests.get(url, auth=auth)
-print(r.json())
+	print(r.json())
+
+fetch_users()
